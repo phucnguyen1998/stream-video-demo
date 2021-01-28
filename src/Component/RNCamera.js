b1ae8a0bf11ba3251ace5ea73d4be384db58b354
phucnguyen1998 <phucnguyen11011998@gmail.com>
2021-01-28 22:24:50 +0700

    add lib react-native-camera

---
+++ src/Component/RNCamera.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    ActivityIndicator,
+    AppRegistry
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+class RnCamera extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            recording: false,
+            processing: false,
+        };
+    }
+
+    async startRecording() {
+        this.setState({ recording: true });
+        // default to mp4 for android as codec is not set
+        const { uri, codec = 'mp4' } = await this.camera.recordAsync();
+        const data = await this.camera.recordAsync();
+        console.log(data);
+    }
+    stopRecording = () => {
+        this.camera.stopRecording();
+    };
+
+    render() {
+        const { recording, processing } = this.state;
+
+        let button = (
+            <TouchableOpacity
+                onPress={this.startRecording.bind(this)}
+                style={styles.capture}>
+                <Text style={{ fontSize: 14 }}> RECORD </Text>
+            </TouchableOpacity>
+        );
+        if (recording) {
+            button = (
+                <TouchableOpacity
+                    onPress={this.stopRecording.bind(this)}
+                    style={styles.capture}>
+                    <Text style={{ fontSize: 14 }}> STOP </Text>
+                </TouchableOpacity>
+            );
+        }
+        if (processing) {
+            button = (
+                <View style={styles.capture}>
+                    <ActivityIndicator animating size={18} />
+                </View>
+            );
+        }
+        return (
+            <View style={styles.container}>
+                <RNCamera
+                    ref={(ref) => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                        title: 'Permission to use audio recording',
+                        message: 'We need your permission to use your audio',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                />
+
+                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                    <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                        {button}
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
+});
+
+AppRegistry.registerComponent('App', () => ExampleApp);
+
+export default RnCamera;
